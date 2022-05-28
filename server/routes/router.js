2de9138fea6af3ba12cdd88f8ca41c743a448fd2
officiallygod <allen.officially@gmail.com>
2022-05-28 22:36:46 +0530

    Fixed ejs files and links

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const route = express.Router()
+const route = express.Router();
 
 const services = require('../services/render');
 const controller = require('../controller/controller');
@@ -14,14 +14,25 @@ route.get('/', services.homeRoutes);
  *  @description add users
  *  @method GET /add-user
  */
-route.get('/add-user', services.add_user)
+route.get('/add-user', services.add_user);
+
+/**
+ *  @description contact
+ *  @method GET /contact
+ */
+route.get('/contact', services.contact);
+
+/**
+ *  @description membership
+ *  @method GET /membership
+ */
+route.get('/membership', services.membership);
 
 /**
  *  @description for update user
  *  @method GET /update-user
  */
-route.get('/update-user', services.update_user)
-
+route.get('/update-user', services.update_user);
 
 // API
 route.post('/api/users', controller.create);
@@ -29,5 +40,4 @@ route.get('/api/users', controller.find);
 route.put('/api/users/:id', controller.update);
 route.delete('/api/users/:id', controller.delete);
 
-
-module.exports = route
+module.exports = route;
