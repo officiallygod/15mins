d88b71a9af0b652adab8ba991fe052b5bb4177e4
officiallygod <allen.officially@gmail.com>
2022-05-28 18:39:13 +0530

    Connected MongoDB

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try{
-        // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
+	try {
+		// mongodb connection string
+		console.log(process.env.MONGO_URI);
+		const con = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		});
 
-        console.log(`MongoDB connected : ${con.connection.host}`);
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
-}
+		console.log(`MongoDB connected : ${con.connection.host}`);
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
